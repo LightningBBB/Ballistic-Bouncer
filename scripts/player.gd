64e147804d374051a28e089df5be11cbfc7f58c5
LightningBB <Crazypenguin919@gmail.com>
2025-07-15 12:35:35 +1200

    added

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,33 @@
 extends CharacterBody2D
-@export var speed = 300
-@export var gravity = 30
-@export var jump_force = 600
 
-func _physics_process(delta: float) -> void:
+@export var speed = 600
+@export var gravity = 30
+@export var jump_force = 800
+
+func _physics_process(delta):
+	# Existing movement code
+	var horizontal_direction = Input.get_axis("move_left", "move_right")
+	velocity.x = speed * horizontal_direction
+
 	if !is_on_floor():
 		velocity.y += gravity
 		if velocity.y > 500:
 			velocity.y = 500
-	
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = -jump_force
-	
-	var horizontal_direction = Input.get_axis("move_left", "move_right")
-	velocity.x = speed * horizontal_direction
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_force
+
 	move_and_slide()
-	
 	print(velocity)
+
+	# Rotate Aim_Indicator with joystick input and toggle visibility
+	var stick_input = Vector2(
+		Input.get_joy_axis(0, JOY_AXIS_RIGHT_X),
+		Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)  # Y-axis NOT inverted
+	)
+
+	if stick_input.length() > 0.1:
+		$Aim_Indicator.visible = true
+		$Aim_Indicator.rotation = stick_input.angle()
+	else:
+		$Aim_Indicator.visible = false

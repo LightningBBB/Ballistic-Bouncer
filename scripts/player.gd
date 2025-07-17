4593ece527ffeca60b75b9b73c381b70c0db0003
LightningBB <Crazypenguin919@gmail.com>
2025-07-17 13:29:42 +1200

    added coloring and weapon controls

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,33 +1,42 @@
 extends CharacterBody2D
 
 @export var speed = 600
-@export var gravity = 50
-@export var jump_force = 800
+@export var gravity = 980
+@export var jump_force = 600
 @export var bulletscene: PackedScene
 
+@export var maxjumps = 2
+var remainingjumps = 2
+
 func _physics_process(delta):
-	# Existing movement codet'
 	var horizontal_direction = Input.get_axis("move_left", "move_right")
 	velocity.x = speed * horizontal_direction
-
-	if  not is_on_floor():
-		velocity.y += gravity
-		if velocity.y > 500:
-			velocity.y = 500
-
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -jump_force
-
-	move_and_slide()
-	print(velocity)	
-
-	# Rotate Aim_Indicator with joystick input and toggle visibility - ChatGPT
+	
 	var stick_input = Vector2(
-		Input.get_joy_axis(0, JOY_AXIS_RIGHT_X),
-		Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)  # Y-axis NOT inverted
+		Input.get_joy_axis(0, JOY_AXIS_LEFT_X),
+		Input.get_joy_axis(0, JOY_AXIS_LEFT_Y)
 	)
 	
-	if Input.is_action_just_pressed("shoot"):
+	if  not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("jump") and remainingjumps > 0:
+		velocity.y = -jump_force
+		remainingjumps -= 1
+		print("Jumped! Remaining jumps:", remainingjumps)
+	
+	move_and_slide()
+	
+	if is_on_floor():
+		remainingjumps = maxjumps
+
+
+	
+	print(velocity)	
+
+
+	
+	if Input.is_action_just_pressed("ranged"):
 		var bullet = bulletscene.instantiate()
 		bullet.rotation = $Aim_Indicator.rotation
 		bullet.position = global_position

bc71c5c2e5e29bacdb164686589403b1cb52a82e
LightningBB <Crazypenguin919@gmail.com>
2025-10-06 11:05:46 +1300

    Added title screen and changed up tutorial level

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -11,16 +11,16 @@ func _ready():
 func take_damage():
 	queue_free()
 
-#func _physics_process(delta):
-	#var direction = player.global_position.x - global_position.x
-	#if direction < 0:
-		#direction = -1
-	#else:
-		#direction = 1
-	#velocity.x = speed * direction
-	#
-	#if  not is_on_floor():
-		#velocity.y += gravity * delta
-	#
-	#move_and_slide()
-	#
+func _physics_process(delta):
+	var direction = player.global_position.x - global_position.x
+	if direction < 0:
+		direction = -1
+	else:
+		direction = 1
+	velocity.x = speed * direction
+	
+	if  not is_on_floor():
+		velocity.y += gravity * delta
+	
+	move_and_slide()
+	
